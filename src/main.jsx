088752ce9090f56212a2b6fb6bad6ba48d5cbaa7
Simon Kununka <skununka@hispuganda.org>
2025-07-17 23:45:16 +0300

    updated all services monkey

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,13 @@ import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom';
 
 // Monkey-patch fetch to prepend /qims to all /api calls
-//  (typeof resource === 'string' && resource.startsWith('/api')) {
-//   const originalFetch = window.fetch;
-// window.fetch = function(resource, options) {
-//   if  resource = '/qims' + resource;
-//   }
-//   return originalFetch(resource, options);
-// };
+ (typeof resource === 'string' && resource.startsWith('/api')) {
+  const originalFetch = window.fetch;
+window.fetch = function(resource, options) {
+  if  resource = '/qims' + resource;
+  }
+  return originalFetch(resource, options);
+};
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
